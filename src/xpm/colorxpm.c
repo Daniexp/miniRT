742fcce595d8ad70b6f61489cd6b058574449758
Daniexp <dani00exp@gmail.com>
2023-02-23 12:04:54 +0100

    move modify_dfclr() to their own .c, to clean it for the norminette

--- src/xpm/colorxpm.c
+++ src/xpm/colorxpm.c
@@ -6,27 +6,11 @@
 /*   By: dexposit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 19:51:49 by dexposit          #+#    #+#             */
-/*   Updated: 2023/02/22 11:20:59 by dexposit         ###   ########.fr       */
+/*   Updated: 2023/02/23 12:03:11 by dexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <miniRT.h>
-
-void	split_free(char **split)
-{
-	char	**aux;
-
-	if (!split)
-		return ;
-	aux = split;
-	while (*aux)
-	{
-		free(*aux);
-		aux++;
-	}
-	free(split);
-	return ;
-}
 
 int	new_clr(t_xpm *xpm, char *rgb)
 {
@@ -61,54 +45,6 @@ int	check_addclr(t_xpm *xpm)
 	if (!xpm)
 		return (-1);
 	return (pow(ft_strlen(BASEXPMCHR), xpm->inf.chpx) > xpm->inf.nclr + 1);
-}
-//añade siemmpre sero  a las definiciones antiguas
-
-char	*modify_dfclr(t_xpm *xpm)
-{
-	char	**olddf;
-	char	*res;
-	char	*aux;
-	char	*auxspc;
-	char	*auxres;
-	int		i;
-
-	if (!xpm)
-		return (NULL);
-	olddf = ft_split (xpm->dfclr, ' ');
-	i = -1;
-	while (olddf[++i])
-	{
-		printf("La combinación inicial es: %s\n", olddf[i]);
-		aux = olddf[i];
-		auxres = ft_strnjoin(BASEXPMCHR[0], aux, 1);
-		free(aux);
-		olddf[i] = auxres;
-		if (olddf[i + 1])
-			i++;
-	}
-	i = -1;
-	res = NULL;
-	while (olddf[++i])
-	{
-		auxspc = olddf[i];
-		auxspc = ft_strjoin(auxspc, " ");
-		aux = ft_strjoin(auxspc, olddf[i + 1]);
-		free(auxspc);
-		auxres = res;
-		res = ft_strjoin(auxres, aux);
-		if (!res)
-			res = ft_substr(aux, 0, ft_strlen(aux));
-		else
-			free(auxres);
-		free(aux);
-		if (olddf[i + 1])
-			i++;
-	}
-	split_free(olddf);
-	free(xpm->dfclr);
-	xpm->dfclr = res;
-	return (res);
 }
 
 char	*create_dfclr(t_xpm *xpm, char *lastdf, char *rgb)
