ded82f36ff034fa681db20342ca68b6346c131b0
NachoDonaire <nacho23donaire@gmail.com>
2023-05-25 14:14:15 +0200

    empezando inside, queda por hacerlo tambien para la luz y color echar ojo II

--- src/parse/iscamera_inside.c
+++ src/parse/iscamera_inside.c
@@ -6,7 +6,7 @@
 /*   By: ndonaire <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 13:26:04 by ndonaire          #+#    #+#             */
-/*   Updated: 2023/05/25 13:33:50 by ndonaire         ###   ########.fr       */
+/*   Updated: 2023/05/25 14:13:56 by ndonaire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,23 @@ int	iscamera_incylinder(t_scene *scene, t_cylinder *cy)
 	bot = pleq(v_gen(cy->vec), v_gen(cy->coord));
 	top = pleq(invert(v_gen(cy->vec)), add_vector(v_gen(cy->coord), mult_k(v_gen(cy->vec), cy->h)));
 	if (subs_in_plane(bot, v_gen(scene->C.coord)) <= EPSILON && subs_in_plane(top, v_gen(scene->C.coord)) <= EPSILON)
+	{
+		if (dot_straight_distance(v_gen(cy->vec), v_gen(cy->coord), v_gen(scene->C.coord)) <= cy->d / 2)
+			return (1);
+	}
+	return (0);
+}
+
+int	iscamera_insphere(t_scene *scene, t_sphere *sp)
+{
+	if (dot_dot_distance(v_gen(scene->C.coord), v_gen(sp->coord)) <= sp->d  / 2)
 		return (1);
+	return (0);
+}
+
+int	iscamera_inplane(t_scene *scene, t_sphere *sp)
+{
+	
 
 
 
